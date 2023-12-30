8075539ef24ca456f5b88b2540a69167d0120262
dalton-baker <dalton.s.baker@gmail.com>
2023-12-30 00:13:59 -0600

    Adding attribute project. Releasing attribute project as analyzer project

---
+++ DepRegAttributes/RegisterScopedAttribute.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DepRegAttributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+#pragma warning disable CS9113 // Parameter is unread.
+public class RegisterScopedAttribute(params Type[] types) : Attribute
+#pragma warning restore CS9113 // Parameter is unread.
+{
+    public object Tag { get; set; }
+
+    public object Key { get; set; }
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2, T3> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2, T3, T4> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2, T3, T4, T5> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2, T3, T4, T5, T6> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2, T3, T4, T5, T6, T7> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2, T3, T4, T5, T6, T7, T8> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2, T3, T4, T5, T6, T7, T8, T9> : RegisterScopedAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterScopedAttribute<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : RegisterScopedAttribute
+{
+}
---
+++ DepRegAttributes/RegisterTransientAttribute.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DepRegAttributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+#pragma warning disable CS9113 // Parameter is unread.
+public class RegisterTransientAttribute(params Type[] types) : Attribute
+#pragma warning restore CS9113 // Parameter is unread.
+{
+    public object Tag { get; set; }
+
+    public object Key { get; set; }
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2, T3> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2, T3, T4> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2, T3, T4, T5> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2, T3, T4, T5, T6> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2, T3, T4, T5, T6, T7> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2, T3, T4, T5, T6, T7, T8> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2, T3, T4, T5, T6, T7, T8, T9> : RegisterTransientAttribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RegisterTransientAttribute<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : RegisterTransientAttribute
+{
+}
